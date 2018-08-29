26dfd502355e644fb67fbfc3b5d946f5a6de4d69
Daniel Ale <denielale9291@gmail.com>
2018-08-29 17:40:21 +0100

    Added Article Model - work in progress

---
+++ models/Article.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
+var slugify = require("slugify");
+
+// Create Schema
+const ArticleSchema = new Schema(
+  {
+    slug: {
+      type: String,
+      lowercase: true,
+      unique: true
+    },
+    title: {
+      type: String
+    },
+    body: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    favoritesCount: {
+      type: String
+    },
+    tagList: {
+      type: String
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "users"
+    }
+  },
+  { timestamps: true }
+);
+
+ArticleSchema.plugin(uniqueValidator, { message: "is already taken" });
+
+ArticleSchema.methods.slugify = () => {
+  this.slug =
+    slugify(this.title) +
+    "-" +
+    ((Math.random() * Math.pow(36, 6)) | 0).toString();
+};
+
+ArticleSchema.methods.toJSONFor = user => {
+  return {
+    slug: this.slug,
+    title: this.title,
+    description: this.description,
+    body: this.body,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt,
+    tagList: this.tagList,
+    favoritesCount: this.favoritesCount,
+    author: user
+  };
+  // author: this.author.toProfileJSONFor(user)
+};
+
+// Let this get called at save time
+ArticleSchema.pre("validate", next => {
+  if (!this.slug) {
+    this.slugify();
+  }
+  next();
+});
+
+module.exports = Article = mongoose.model("articles", ArticleSchema);
