acdc286e00e307dbea2a2d367f96df7be222cb74
Daniel Ale <denielale9291@gmail.com>
2018-08-30 09:43:50 +0100

    Updated article PUT and create route

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -35,6 +35,11 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    User.findById(req.user.id).then(user => {
+      if (!user) {
+        return res.status(401).json({ errors: "user not found" });
+      }
+    });
     const { errors, isValid } = validateArticleInput(req.body);
     // Check Validation
     if (!isValid) {
@@ -60,6 +65,71 @@ router.post(
       })
       .catch(err => {
         console.log(err);
+      });
+  }
+);
+
+// @route   GET api/article/:slug
+// @desc    Get Article
+// @access  Public
+router.get("/:slug", (req, res) => {
+  const errors = {};
+  Article.findOne({ slug: req.params.slug }).then(article => {
+    if (!article) {
+      errors.noarticle = "Article not found";
+      return res.status(404).json(errors);
+    }
+
+    return res.json(article);
+  });
+});
+
+// @route   PUT api/article/:slug
+// @desc    Update Article
+// @access  Private
+router.put(
+  "/:slug",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    User.findById(req.user.id).then(user => {
+      if (!user) {
+        return res.status(401).json({ errors: "user not found" });
+      }
+    });
+    const { errors, isValid } = validateArticleInput(req.body);
+    // Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    const articleFields = {
+      title: req.body.title,
+      description: req.body.description,
+      body: req.body.body,
+      tagList: req.body.tagList
+    };
+
+    Article.findOne({ slug: req.params.slug })
+      .then(article => {
+        if (!article) {
+          errors.articlenotfound = "Article not found";
+          return res.status(404).json(errors);
+        }
+
+        Article.findOneAndUpdate(
+          { slug: req.params.slug },
+          { $set: articleFields },
+          { new: true }
+        ).then(article => {
+          if (!article) {
+            errors.updateerror = "There is an error updating article";
+            return res.status(405).json(errors);
+          }
+          return res.json(article);
+        });
+      })
+      .catch(err => {
+        errors.articlenotfound = "Article can't be updated";
+        return res.status(500).json(errors);
       });
   }
 );
