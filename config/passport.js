07c35653b9479a71a6dff3aa097055b22917011b
Daniel Ale <denielale9291@gmail.com>
2018-09-14 12:49:57 +0100

    Set up local passport

--- config/passport.js
+++ config/passport.js
@@ -1,25 +1,26 @@
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-const User = mongoose.model("users");
-const keys = require("./keys");
+var User = mongoose.model("User");
 
-const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretOrKey;
-
-module.exports = passport => {
-  passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {
-      // console.log(jwt_payload);
-      User.findById(jwt_payload.id)
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "user[email]",
+      passwordField: "user[password]"
+    },
+    (email, password, done) => {
+      User.findOne({ email: email })
         .then(user => {
-          if (user) {
-            return done(null, user);
+          if (!user || !user.validPassword(password)) {
+            return done(null, false, {
+              errors: { "email or password": "is invalid" }
+            });
           }
-          return done(null, false);
+
+          return done(null, user);
         })
-        .catch(err => console.log(err));
-    })
-  );
-};
+        .catch(done);
+    }
+  )
+);
